85f40d1e7d4ec0a47f626a741130c28ce0aec2d0
Ryan Barley <ryan@Ryans-MacBook-Pro.local>
2023-07-17 23:36:32 -0400

    Implement data transformation

---
+++ src/utils.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+import numpy as np
+import pandas as pd
+import pickle
+
+from src.exception import CustomException
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file:
+            pickle.dump(obj, file)
+
+    except Exception as e:
+        CustomException(e, sys)
